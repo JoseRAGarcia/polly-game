9a6fd807a2bec937ab19e7ddaedff68f833947bc
José Rosaldo Araujo Garcia <jose.ragarcia82@gmail.com>
2023-06-18 20:14:17 -0300

    Add files via upload

---
+++ components/Player.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="player-container">
+        <img :src="`assets/img/characters/polly/moving-${direction}-${position}.png`" alt="">
+    </div>
+</template>
+
+<script setup>
+import {ref, onMounted, onUnmounted} from 'vue'
+
+const props = defineProps({
+    direction: {
+        type: String,
+        default: 'up'
+    },
+})
+
+onMounted(() => {
+  timer = setInterval(() => {
+    position.value < 3 ? position.value++ : position.value = 1
+  }, 300);
+})
+
+onUnmounted(() => {
+  clearInterval(timer)
+  timer.value = null
+})
+
+const position = ref(1)
+let timer = ref(null)
+</script>
+
+<style scoped>
+.player-container {
+    width: 100px;
+    height: 100px;
+    overflow: hidden;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.player-container img {
+    max-width: 100%;
+    max-height: 100%;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="game-container" tabindex="0" @keydown="movePlayer">
+    <img src="assets/img/landscapes/yard.jpg" alt="">
+    <Player
+      class="player"
+      :direction="playerData.direction"
+      :style="`top: ${coord.top}px; left: ${coord.left}px; `"
+    />
+  </div>
+</template>
+
+<script setup>
+// https://www.pngegg.com/en/png-bzbpq
+
+import { reactive } from "vue";
+
+const coord = reactive({
+  top: 0,
+  left: 0,
+});
+
+const options  = reactive({
+  speed: 5,
+});
+
+const playerData = reactive({
+  direction: 'up',
+});
+
+function movePlayer(event) {
+  if (event.code === "ArrowUp" || event.code === "KeyW") {
+    playerData.direction = 'up'
+    coord.top-=options.speed;
+  } else if (event.code === "ArrowRight" || event.code === "KeyD") {
+    playerData.direction = 'right'
+    coord.left+=options.speed;
+  } else if (event.code === "ArrowDown" || event.code === "KeyS") {
+    playerData.direction = 'down'
+    coord.top+=options.speed;
+  } else if (event.code === "ArrowLeft" || event.code === "KeyA") {
+    playerData.direction = 'left'
+    coord.left-=options.speed;
+  }
+}
+</script>
+
+<style scoped>
+.game-container {
+  position: relative;
+  background: green;
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+}
+.game-container img {
+    width: 100%;
+    height: 100%;
+}
+
+.player {
+  position: absolute;
+  left: 50%;
+}
+</style>
+
